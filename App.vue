ddda6095b44fbfb1a939be54ea144b0114c5ff3d
zzy-dsy <145020473+zzy-dsy@users.noreply.github.com>
2023-09-15 19:14:26 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { computed,ref } from 'vue';
+
+const list =ref( [1,2,3,4,5,6,7,8] )
+
+const computedlist = computed(() =>{
+
+  return list.value.filter(item=> item>=2)
+})
+
+const added =() =>{
+  list.value.push(123)
+}
+</script>
+
+<template>
+  <div>原来的数组  ，{{list}}</div>
+  <div>修改后的数组  ，{{ computedlist }}</div>
+
+  <button @click="added">+123</button>
+</template>
